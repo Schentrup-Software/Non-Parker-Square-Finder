ba950dbf552ad0ac038db9b43f3d47d060bf5694
Joseph Schentrup <j03y@live.com>
2023-06-16 08:56:20 -0400

    Get GPU support working

---
+++ Extentions.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NonParkerSquareFinder
+{
+    public static class Extentions
+    {
+        public static IEnumerable<(T, T)> GetAllPairs<T>(this IEnumerable<T> enumerable)
+        {
+            if (enumerable.Count() <= 2)
+            {
+                yield return (enumerable.First(), enumerable.Last());
+            }
+            else
+            {
+                var firstItem = enumerable.FirstOrDefault();
+                var newEnumerable = enumerable.Skip(1);
+
+                foreach (var otherValue in newEnumerable)
+                {
+                    yield return (firstItem, otherValue);
+                }
+
+                foreach (var subcallValue in GetAllPairs(newEnumerable))
+                {
+                    yield return subcallValue;
+                }
+            }
+        }
+    }
+}
