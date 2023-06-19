ed93c90638646269f3f32432e91573f77905e4bf
Joseph Schentrup <j03y@live.com>
2023-06-19 16:03:01 -0400

    Finish up the Scalar Transition

---
+++ cSharp/Cache.cs
@@ -0,0 +1,60 @@
+﻿using Silk.NET.Maths;
+using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NonParkerSquareFinder
+{
+    internal class Cache
+    {
+        const string filePath = "./xyfile.txt";
+
+        public static async Task<ConcurrentQueue<(ulong, ulong)>> PullFromCache(List<ulong> searchSpace)
+        {
+            var xfile = File.Exists(filePath) ? await File.ReadAllLinesAsync(filePath) : Array.Empty<string>();
+            var highestMExists = ulong.TryParse(xfile.FirstOrDefault(), out var highestM);
+
+            var newSearchSpace = highestMExists ? new ConcurrentQueue<(ulong, ulong)>(xfile.Skip(1).Select(x =>
+            {
+                var split = x.Split(',').Select(y => ulong.Parse(y));
+                return (split.First(), split.Last());
+            })) : new ConcurrentQueue<(ulong, ulong)>();
+
+            var searchSpaceTasks = searchSpace
+                .Where(m => m > highestM)
+                .Select(m => Task.Run(() =>
+                {
+                    var maxX = m / 2;
+
+                    for (ulong x = 1; x < maxX; x++)
+                    {
+                        var mPlusX = m + x;
+                        var mMinusX = m - x;
+
+                        var sqrtMPlusX = Scalar.Sqrt(mPlusX);
+
+                        if (Scalar.Pow<ulong>(sqrtMPlusX, 2) == mPlusX)
+                        {
+                            continue;
+                        }
+
+                        var sqrtMMinusX = Scalar.Sqrt(mMinusX);
+
+                        if (Scalar.Pow<ulong>(sqrtMMinusX, 2) == mMinusX) {
+                            newSearchSpace.Enqueue((m, x));
+                        }
+                    }
+                }));
+
+            await Task.WhenAll(searchSpaceTasks);
+
+            File.Delete(filePath);
+            await File.AppendAllLinesAsync(filePath, newSearchSpace.Select(x => $"{x.Item1},{x.Item2}").Prepend(searchSpace.Max().ToString()));
+
+            return newSearchSpace;
+        }
+    }
+}
---
+++ cSharp/Program.cs
@@ -0,0 +1,84 @@
+﻿#define SSE
+
+using NonParkerSquareFinder;
+using Silk.NET.Maths;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+const string mfilePath = "./mfile.txt";
+const ulong totalSearchSpace = long.MaxValue / 10_000_000_000_00;
+
+
+var sw = new Stopwatch();
+sw.Start();
+
+var searchSpace = new List<ulong>();
+
+ulong maxCounter = Scalar.Sqrt(totalSearchSpace);
+ulong counter = 3;
+
+while (true)
+{
+    searchSpace.Add(counter * counter++);
+
+    if (maxCounter <= counter)
+    {
+        break;
+    }
+}
+
+File.Delete(mfilePath);
+await File.AppendAllLinesAsync(mfilePath, searchSpace.Select(x => x.ToString()));
+
+Console.WriteLine(searchSpace.Count);
+
+var newSearchSpace = (await Cache.PullFromCache(searchSpace))
+    .GroupBy(x => x.Item1, x => x.Item2)
+    .Where(x => x.Count() > 1);
+
+Console.WriteLine(newSearchSpace.Count());
+
+var endTasks = newSearchSpace.Select(group => Task.Run(() =>
+{
+    var pairedGroups = group.GetAllPairs();
+
+    foreach(var pairs in pairedGroups)
+    {
+        var x = pairs.Item1;
+        var y = pairs.Item2;
+        var m = group.Key;
+
+        if (x + y > m)
+        {
+            return;
+        }
+
+        var tests = new ulong[] { (m + (x + y)), (m - (x + y)), (m + (x - y)), (m - (x - y)) };
+        var isGood = true;
+
+        foreach (var test in tests) 
+        {
+            var sqrtTest = Scalar.Sqrt(test);
+            if (isGood && Scalar.Pow<ulong>(sqrtTest, 2) == test)
+            {
+                continue;
+            }
+
+            isGood = false;
+        }
+
+        if (isGood)
+        {
+            Console.WriteLine($"Found it: m = {m}, x = {x}, y = {y}");
+        }
+    }
+}));
+
+await Task.WhenAll(endTasks);
+sw.Stop();
+
+Console.WriteLine($"None found. Time {sw.Elapsed.Minutes} min. {sw.Elapsed.Seconds} sec.");
